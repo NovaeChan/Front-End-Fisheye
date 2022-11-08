20dab0d040505b446cbb547617922bc9d3ab4026
Novae <cedricboutillier59@gmail.com>
2022-11-08 02:35:40 +0100

    integrated home page's basic features

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,47 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { id, name, portrait, city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
-
+    
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.id = id;
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.style.borderRadius = "50%";
+        img.alt = `Photo de profil de ${name}`;
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        //Creer div + ajouter les p à la div
+        const blockDescription = document.createElement( 'div' );
+        blockDescription.style.textAlign = 'center';
+        blockDescription.style.margin = '-10px';
+
+        const locations = document.createElement( 'p' );
+        locations.textContent = `${city}, ${country}`;
+        locations.style.color = "#901C1C";
+        locations.classList.add('country');
+
+        const tag = document.createElement( 'p' );
+        tag.classList.add('tagline');
+        tag.textContent = `${tagline}`;
+
+        const cost = document.createElement( 'p' );
+        cost.classList.add('price');
+        cost.style.fontStyle = "italic";
+        cost.style.color = "#525252";
+        cost.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        blockDescription.appendChild(locations);
+        blockDescription.appendChild(tag);
+        blockDescription.appendChild(cost);
+
+        article.appendChild(blockDescription);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,18 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        try {
+            const response = await fetch('./data/photographers.json');
+            const photographers = await response.json();
+            const onlyPhotographers = photographers.photographers;
+            console.log(onlyPhotographers);
+            return onlyPhotographers;
+        } catch (error) {
+            console.error(`An error occured : ${error}`);
+        }
+        
+        // return ({
+        //     photographers: [...onlyPhotographers]
+        // });
     }
 
     async function displayData(photographers) {
@@ -37,9 +27,9 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const photographers = await getPhotographers();
+        console.log(photographers);
         displayData(photographers);
-    };
+    }
     
     init();
-    
